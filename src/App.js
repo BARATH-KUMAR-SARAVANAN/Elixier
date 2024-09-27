dc8d6857eec675dffdb9f508615e4793b4124066
barathKumar <barathkumarwodaiyar@gmail.com>
2024-09-27 06:46:03 +0530

    added the each event details but styling is not done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {  Component } from 'react'
 import Home from './Components/Home'
 import About from './Components/About'
 import Events from './Components/Events'
 import Contact from './Components/Contact'
 import TechEvents from './Components/TechEvents'
 import NonTechEvents from './Components/NonTechEvents'
-
+import EventDetailedView from './Components/EventDetailedView'
 import './App.css';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route exact path="/" element={<Home/>} />
-      <Route exact path="/about" element={<About/>} />
-      <Route exact path="/events" element={<Events/>} />
-      <Route exact path="/contact" element={<Contact/>} />
-      <Route exact path="/events/tech_events" element={<TechEvents/>} />
-      <Route exact path="/events/non_tech_events" element={<NonTechEvents/>} />
-    </Routes>
-    </BrowserRouter>
-  )
+class App extends Component {
+  
+  render(){
+    return (
+      <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/events" element={<Events/>} />
+        <Route exact path="/contact" element={<Contact/>} />
+        <Route exact path="/events/tech_events" element={<TechEvents/>} />
+        <Route exact path="/events/tech_events/:id" element={<EventDetailedView/>} />
+        <Route exact path="/events/non_tech_events" element={<NonTechEvents/>} />
+        
+      </Routes>
+      </BrowserRouter>
+      
+    )
+  }
 }
 
 export default App;
--- src/Components/EventDetailedView/index.js
+++ src/Components/EventDetailedView/index.js
@@ -1,12 +1,8 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
 import Navbar from '../Navbar'
 import './index.css'
-import { Link } from 'react-router-dom'
-
-
-const techEvents =[
-  {
-    id: 'PaperPresentation',
+const techEvents = {
+  PaperPresentation: {
     event: 'Paper Presentation',
     imgUrl:'https://res.cloudinary.com/dt9nmt0n0/image/upload/v1682769737/pexels-pixabay-53464_pzgmds.jpg',
     googleFormLink:'https://www.youtube.com/',
@@ -22,42 +18,8 @@ const techEvents =[
       'Be respectful during the Q&A session, even if the questions are challenging. Respond clearly and concisely.',
       'Always carry a backup copy of your presentation on a USB and/or cloud storage.'
     ]
-  },{
-    id:'ProjectPresentation',
-    event: 'Project Presentation',
-    imgUrl:'https://res.cloudinary.com/dt9nmt0n0/image/upload/v1682769737/pexels-pixabay-53464_pzgmds.jpg',
-    googleFormLink:'https://www.youtube.com/',
-    rules:
-    [
-      "Stick to the allotted time, usually 10-15 minutes for the presentation and 5 minutes for Q&A.",
-      'Ensure your presentation has a clear structure—Introduction, Methodology, Results, Conclusion.',
-      'Use minimal text per slide, focusing on bullet points and visuals (graphs, diagrams, images) over paragraphs.',
-      'Present complex ideas simply; avoid jargon unless necessary, and define any technical terms.',
-      'Dress professionally and maintain good posture. Be confident in your delivery and maintain eye contact.',
-      'Ask thought-provoking questions to the audience or use real-world examples to keep them engaged.',
-      'Mention sources and references where necessary to establish credibility.',
-      'Be respectful during the Q&A session, even if the questions are challenging. Respond clearly and concisely.',
-      'Always carry a backup copy of your presentation on a USB and/or cloud storage.'
-    ]
-  },{
-    id: 'PaperPresentation2',
-    event: 'Paper Presentation',
-    imgUrl:'https://res.cloudinary.com/dt9nmt0n0/image/upload/v1682769737/pexels-pixabay-53464_pzgmds.jpg',
-    googleFormLink:'https://www.youtube.com/',
-    rules:
-    [
-      "Stick to the allotted time, usually 10-15 minutes for the presentation and 5 minutes for Q&A.",
-      'Ensure your presentation has a clear structure—Introduction, Methodology, Results, Conclusion.',
-      'Use minimal text per slide, focusing on bullet points and visuals (graphs, diagrams, images) over paragraphs.',
-      'Present complex ideas simply; avoid jargon unless necessary, and define any technical terms.',
-      'Dress professionally and maintain good posture. Be confident in your delivery and maintain eye contact.',
-      'Ask thought-provoking questions to the audience or use real-world examples to keep them engaged.',
-      'Mention sources and references where necessary to establish credibility.',
-      'Be respectful during the Q&A session, even if the questions are challenging. Respond clearly and concisely.',
-      'Always carry a backup copy of your presentation on a USB and/or cloud storage.'
-    ]
-  },{
-    id: 'ProjectPresentation2',
+  },
+  ProjectPresentation: {
     event: 'Project Presentation',
     imgUrl:'https://res.cloudinary.com/dt9nmt0n0/image/upload/v1682769737/pexels-pixabay-53464_pzgmds.jpg',
     googleFormLink:'https://www.youtube.com/',
@@ -74,31 +36,58 @@ const techEvents =[
       'Always carry a backup copy of your presentation on a USB and/or cloud storage.'
     ]
   },
-]
-
-const TechEvents = () => {
-  return (
-    <div className='tech-events-page'>
-      <Navbar/>
-      <div className='tech-events-container'>
-        <h1 className='tech-event-heading'>Technical Events</h1>
-        <ul className='event-holder'>
-          {
-            techEvents.map(each =>{ 
-              return(
-            <li key={each.id} className='event-card'>
-              <h1 className='heading'>{each.event}</h1>
-              <img className='event-images' src={each.imgUrl} alt={each.event} />
-                <Link  to={`/events/tech_events/${each.id}`}>
-                  <button className='moreDetail-button'>More Detail</button>
-                </Link>
-            </li>)}
-            )
-          }
-        </ul>
-      </div>
-    </div>
-  )
+  PaperPresentation2:{
+    event: 'Paper Presentation2',
+    imgUrl:'https://res.cloudinary.com/dt9nmt0n0/image/upload/v1682769737/pexels-pixabay-53464_pzgmds.jpg',
+    googleFormLink:'https://www.youtube.com/',
+    rules:
+    [
+      "Stick to the allotted time, usually 10-15 minutes for the presentation and 5 minutes for Q&A.",
+      'Ensure your presentation has a clear structure—Introduction, Methodology, Results, Conclusion.',
+      'Use minimal text per slide, focusing on bullet points and visuals (graphs, diagrams, images) over paragraphs.',
+      'Present complex ideas simply; avoid jargon unless necessary, and define any technical terms.',
+      'Dress professionally and maintain good posture. Be confident in your delivery and maintain eye contact.',
+      'Ask thought-provoking questions to the audience or use real-world examples to keep them engaged.',
+      'Mention sources and references where necessary to establish credibility.',
+      'Be respectful during the Q&A session, even if the questions are challenging. Respond clearly and concisely.',
+      'Always carry a backup copy of your presentation on a USB and/or cloud storage.'
+    ]
+  },
+  ProjectPresentation2:{
+    event: 'Project Presentation2',
+    imgUrl:'https://res.cloudinary.com/dt9nmt0n0/image/upload/v1682769737/pexels-pixabay-53464_pzgmds.jpg',
+    googleFormLink:'https://www.youtube.com/',
+    rules:
+    [
+      "Stick to the allotted time, usually 10-15 minutes for the presentation and 5 minutes for Q&A.",
+      'Ensure your presentation has a clear structure—Introduction, Methodology, Results, Conclusion.',
+      'Use minimal text per slide, focusing on bullet points and visuals (graphs, diagrams, images) over paragraphs.',
+      'Present complex ideas simply; avoid jargon unless necessary, and define any technical terms.',
+      'Dress professionally and maintain good posture. Be confident in your delivery and maintain eye contact.',
+      'Ask thought-provoking questions to the audience or use real-world examples to keep them engaged.',
+      'Mention sources and references where necessary to establish credibility.',
+      'Be respectful during the Q&A session, even if the questions are challenging. Respond clearly and concisely.',
+      'Always carry a backup copy of your presentation on a USB and/or cloud storage.'
+    ]
+  },
 }
 
-export default TechEvents
+const EventDetailedView = () => {
+  const params = useParams();
+  const eventId =params.id;
+  const eve = techEvents[eventId]
+    return(
+      <div>
+        <Navbar/>
+        <div>
+          <h1>{eve.event}</h1>
+          <ul>
+            {
+              eve.rules.map(each => <li key={each}>{each}</li>)
+            }
+          </ul>
+        </div>
+      </div>
+    )
+}
+export default EventDetailedView
